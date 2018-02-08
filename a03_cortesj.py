728a33031a7c2a0116486017e98a8c33c217a610
cortesj <cortesj@berea.edu>
2018-02-07 21:27:06 -0500

    Removed A03_cortesj.py and Windows_XP_Bliss.jpg

---
+++ a03_cortesj.py
@@ -0,0 +1,39 @@
+#################################################################################
+# Jose Cortes
+# username: Cortesj
+
+# Assignment: A03
+#################################################################################
+
+import turtle
+
+
+def function_1():
+    """
+    Docstring for function_1
+    :return:
+    """
+    pass
+
+
+def function_2():
+    """
+    Docstring for function_2
+    :return:
+    """
+    pass
+
+
+def main():
+    """
+    Docstring for Main
+    :return:
+    """
+    wn = turtle.Screen()
+    wn.bgpic("Windows_XP_Bliss.jpg")
+    wn.screensize(500, 500)
+    wn.colormode((0, 0, 0))
+    wn.exitonclick()
+
+
+main()
