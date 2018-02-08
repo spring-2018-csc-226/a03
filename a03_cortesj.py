64f9dda18cd0b03afc0486d12cab113a0e14d2e7
cortesj <cortesj@berea.edu>
2018-02-07 20:58:23 -0500

    Added file "a03_cortesj.py" for A03 Assignment

---
+++ a03_cortesj.py
@@ -0,0 +1,35 @@
+#################################################################################
+# Jose Cortes
+# username: Cortesj
+
+# Assignment: A03
+#################################################################################
+
+import turtle
+
+
+def function_1():
+    """
+    Docstring for function_1
+    :return:
+    """
+    pass
+
+
+def function_2():
+    """
+    Docstring for function_2
+    :return:
+    """
+    pass
+
+
+def main():
+    """
+    Docstring for Main
+    :return:
+    """
+    pass
+
+
+main()
